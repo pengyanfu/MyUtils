0c394e002063a78cf7bf04004e03cbce08bafb94
彭彭彭 <986810471@qq.com>
2017-04-14 11:28:51 +0800

    414

---
+++ app/src/main/java/com/yonyou/pyf/myutils/activities/ScrollingActivity.kt
@@ -0,0 +1,26 @@
+package com.yonyou.pyf.myutils.activities
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+
+import com.yonyou.pyf.myutils.R
+
+class ScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener(View.OnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        })
+    }
+}
